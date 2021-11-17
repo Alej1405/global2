0823ffcb4cc04017596a45cd7b416c9d2f6c5f34
Mashawebcorp <lineasdec@gmail.com>
2021-11-17 00:07:07 -0500

    GLOBSL_1.1

--- globalcargo/globalcargo.php
+++ globalcargo/globalcargo.php
@@ -45,26 +45,22 @@
               </div>
           </div>
       </div>
-      <div class="opciones">
-          <div class="botones2">
-              <a href="imporacion.php" class="opc">COTIZAR IMPORTACIÓN</a>
-              <a href="imporacion.php" class="opc">COTIZAR EXPORTACIÓN</a>
-          </div>
-          <div class="imgopc">
-              <img src="../IMG\Barco.png" alt="transporte maritimo de carga" class="imgopc1">
-          </div>
+      <div>
+        <div class="contacto">
+          <p> 
+            Somos una empresa dedicada a brindar
+            servicios de Comercio Exterior 
+            y Logística internacional,
+            enfocados siempre en la Agilidad y calidad 
+            en cada uno de nuestros procesos.
+          </p>
+        </div>
+        <div class="contacto">
+            <?php incluirTemplate('form') ?>
+        </div>
       </div>
     </main>
     <footer class="final">
-      <!-- formulario insertado desde el CRM -->
-      <div class="cont">
-        <div class="contacto">
-          <?php incluirTemplate('form') ?>
-        </div>
-        <div class="ubicacion">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7619577206556!2d-78.45564398524667!3d-0.30454899977779953!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d5bd871b7edfaf%3A0x1c1e60c2f02a3597!2sSan%20Rafael%20Business%20Center!5e0!3m2!1ses!2sec!4v1632519679475!5m2!1ses!2sec" style="border:0;" allowfullscreen="" loading="lazy" class="mapa"></iframe>
-        </div>
-      </div>
       <div class="pie">
         <div class="logopie">
           <a href="index.php"><img src="../IMG\global.png" alt="logo global cargo" class="logop"></a>

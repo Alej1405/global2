9f70111d36e2fdfcb293a338cb4d54ab1ccd4eeb
Alej1405 <pablorevillafactos@hotmail.com>
2022-07-21 12:39:23 -0500

    agregar tarifas formulario y consulta completa

--- sistema/USUARIOS/tarifas_paqueteria.php
+++ sistema/USUARIOS/tarifas_paqueteria.php
@@ -27,6 +27,47 @@ $db3 = conectarDB3();
 //CONECCION CALLCENTER
 conectarDB4();
 $db4 = conectarDB4();
+//----------------DECLARAR VARIABLES DEL SISTEM Y CONSTANTES DEL SERVIDOR----------------
+    $tarifa = "";
+    $valor = "";
+    $peso_maximo = "";
+    $valor_extra = "";
+    $errores = [];
+    $error = '';
+
+//----------------CAPTURA DE DATOS METODO POST----------------
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $tarifa = $_POST['tarifa'];
+    $valor = $_POST['valor'];
+    $peso_maximo = $_POST['peso_maximo'];
+    $valor_extra = $_POST['valor_extra'];
+    $responsable = $_SESSION['usuario'];
+    $fecha = date('y-m-d');
+
+    if(!$tarifa) {
+        $errores[] = "NOOOO!!!! POR FAVOR INGRESA UNA TARIFA.....";
+    }
+    if(!$valor) {
+        $errores[] = "UTAAAAA!!!! FALTA EL VALOR PUES.....";
+    }
+    if(!$peso_maximo) {
+        $errores[] = "NO SE PUEDE ASI!!!! FALTA EL PESO MAXIMO.....";
+    }
+    if(!$valor_extra) {
+        $errores[] = "ELEEEEE QUESF!!!! PON EL VALOR EXTRA SI NO COMO?.....";
+    }
+    if(empty($errores)){
+        $g_tarifa = "INSERT INTO tarifas (nombre, valor, peso, valor_extra, responsable, fecha)
+                                    values('$tarifa', '$valor', '$peso_maximo', '$valor_extra', '$responsable', '$fecha');";
+        $ej_tarifa = mysqli_query($db4, $g_tarifa);
+        if ($ej_tarifa){
+            $error = 1;
+
+        }
+
+    }
+}
 
 ?>
 
@@ -34,103 +75,78 @@ $db4 = conectarDB4();
     <div class="container">
         <!-- FORMULARIO DE ACTUALIZACION -->
             <div class="card bg-light">
-                    <?php //foreach($errores as $error) : ?>
+                    <?php foreach($errores as $error) : ?>
                         <div class="alert alert-danger">
-                            <?php //echo $error; ?>
+                            <?php echo $error; ?>
                         </div>
-                    <?php //endforeach ?>
-                <div class="card-header">
-                    REGISTRO Y MOVIMIENTO FINANCIERO
+                    <?php endforeach ?>
+                    <?php if(intval($error) === 1 ): ?>
+                        <p class="alert alert-success">VES!!!! FACIL ES HAGALE, HAGALE...</p>
+                    <?php endif ?>
+                <div class="alert alert-primary">
+                    INGRESAR TARIFAS DE PAQUETERIA
                 </div>
                 <form action ='' method="POST" enctype="multipart/form-data">
                     <br>
-                    <div class="input-group mb-3">
-                        <select class="form-select form-select-sm" name="tipo" aria-label=".form-select-sm example">
-                            <option selected>selecciona un tipo de Transaccion</option>
-                            <option value="Ingreso">Ingreso</option>
-                            <option value="Egreso">Egreso</option>
-                        </select>
+                    <div class="mb-3">
+                        <label for="tarifa" class="form-label">Nombre de la tarifa...</label>
+                        <input type="text" placeholder="Ingresar el nombre solo en mayusculas" id="tarifa" accept="pdf" name="tarifa" class="form-control" require aria-label="Username" aria-describedby="basic-addon1">
                     </div>
-                    
                     <div class="input-group mb-3">
                         <span class="input-group-text">Valor $</span>
                         <input type="float" class="form-control" name="valor" aria-label="Amount (to the nearest dollar)">
-                        <span class="input-group-text"></span>
                     </div>
-
                     <div class="input-group mb-3">
-                        <span class="input-group-text">Valor sin IVA $</span>
-                        <input type="float" class="form-control" name="valor_SV" aria-label="Amount (to the nearest dollar)" value="0">
-                        <span class="input-group-text"></span>
+                        <span class="input-group-text">Peso maximo...</span>
+                        <input type="float" placeholder="Ingresar en KILOS si hay decimales con . no con , " class="form-control" name="peso_maximo" aria-label="Amount (to the nearest dollar)" >
                     </div>
-
-                    <div class="mb-3">
-                        <label for="respaldo" class="form-label">Factura o Respaldo</label>
-                        <input type="file" id="respaldo" accept="pdf" name="respaldo" class="form-control" require aria-label="Username" value="Adjuntar Respaldo" aria-describedby="basic-addon1">
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="deposito" class="form-label">Comprobanto o Respaldo</label>
-                        <input type="file" id="deposito" accept="pdf" name="deposito" class="form-control" require aria-label="Username" value="Adjuntar Respaldo" aria-describedby="basic-addon1">
-                    </div>
-
                     <div class="input-group mb-3">
-                        <select class="form-select form-select-sm" name="detalle" aria-label=".form-select-sm example">
-                            <option selected>selecciona un detalle</option>
-                            <option value="anticipo">Anticipo</option>
-                            <option value="pago proveedores">Pago de proveedores</option>
-                            <option value="servicios basicos uio">Servicios Basicos UIO</option>
-                            <option value="servicios basicos gye">Servicios Basicos GYE</option>
-                            <option value="gastos administrativos">Gastos Administrativos</option>
-                            <option value="viaticos">Viaticos</option>
-                            <option value="gastos varios">Gastos Varios</option>
-                            <option value="insumos de oficina">Insumos de Oficina</option>
-                            <option value="gasto operativo">Gasto Operativo</option>
-                            <option value="transporte">Transporte</option>
-                            <option value="gasto administrativo">Gasto Administrativo</option>
-                        </select>
+                        <span class="input-group-text">Valor extra por kilo...</span>
+                        <input type="float" placeholder="Ingresar, si hay decimales con . no con , " class="form-control" name="valor_extra" aria-label="Amount (to the nearest dollar)">
                     </div>
-
-                    <div class="input-group mb-3">
-                        <select class="form-select form-select-sm" name="banco" aria-label=".form-select-sm example">
-                            <option selected>selecciona una cuenta BANCARIA</option>
-                            <option value="bco. de guayaquil">Bco. Guayaquil</option>
-                            <option value="bco produbanco">Bco. Produbanco</option>
-                            <option value="otra cuenta">Otra Cuenta</option>
-                        </select>
+                    <br>
+                    <div class="d-grid gap-2 col-6 mx-auto">
+                        <input type="submit" value="INGRESAR TARIFA" class="btn btn-outline-primary bi-text-center">
                     </div>
-
-                    <div class="input-group mb-3">
-                        <select class="form-select form-select-sm" name="cuenta" aria-label=".form-select-sm example">
-                            <option selected>selecciona una cuenta contable</option>
-                            <option value="c. por pagar">C. Por Pagar</option>
-                            <option value="c. por cobrar">C. Por Cobrar</option>
-                            <option value="gasto operativo">Gasto Operativo</option>
-                            <option value="caja chica">Caja Chica</option>
-                            <option value="caja general">Caja General</option>
-                        </select>
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <select class="form-select form-select-sm" name="factura" aria-label=".form-select-sm example">
-                            <option selected>Es una factura..?</option>
-                            <option value="si">Si</option>
-                            <option value="no">No</option>
-                        </select>
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Observacion</span>
-                        <textarea class="form-control" name="observacion" aria-label="With textarea"></textarea>
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <input type="submit" value="Guardar Deposito" class="btn btn-outline-primary">
-                    </div>
+                    <br>
                 </form>
             </div>
         <!-- FIN DE FORMULARIO DE ACTUALIZACION -->
     </div>
+    <br>
+    <br>
+    <div class="container">
+        <div class="alert alert-primary">
+            <h4 class="">TARIFAS INGRESADAS</h4>
+        </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>TARIFA</th>
+                <th>VALOR</th>
+                <th>PESO MAXIMO</th>
+                <th>V EXTRA POR KILO</th>
+                <th>FECHA DE REGISTRO</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            $consulta = "SELECT * FROM tarifas";
+            $eje_consulta = mysqli_query($db4, $consulta);
+            while($tarifas = mysqli_fetch_assoc($eje_consulta)):?>
+            <tr>
+                <td><?php echo $tarifas['nombre'];?></td>
+                <td><?php echo $tarifas['valor'];?></td>
+                <td><?php echo $tarifas['peso'];?></td>
+                <td><?php echo $tarifas['valor_extra'];?></td>
+                <td><?php echo $tarifas['fecha'];?></td>
+            </tr>
+            <?php endwhile;?>
+        </tbody>
+    </table>
+    </div>
+    <br>
+    <br>
 <?php 
     incluirTemplate('fottersis');
 ?>
